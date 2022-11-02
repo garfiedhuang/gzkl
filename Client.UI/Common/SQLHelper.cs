1f4ad94aee989c422dcd5c2ceec87e40da18e41d
garfield <google_2009@163.com>
2022-11-02 14:33:31 +0800

    新增SQLHelper帮助类

---
+++ Client.UI/Common/SQLHelper.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace GZKL.Client.UI.Common
+{
+    /// <summary>
+    /// 数据库的通用访问代码 抽象类
+    /// 不允许实例化，在应用时直接调用即可
+    /// </summary>
+    public abstract class SQLHelper
+    {
+
+        private static string connStr = ConfigurationManager.ConnectionStrings["dbConnStr"].ConnectionString;
+
+        /// <summary>
+        /// 执行非查询语句
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static int ExecuteNonQuery(string sql, SqlParameter[] parameters = null)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    if (parameters != null && parameters.Count() > 0)
+                    {
+                        cmd.Parameters.AddRange(parameters);
+                    }
+                    return cmd.ExecuteNonQuery();//返回的是值是insert,delete,update影响了多少行
+                }
+            }
+        }
+
+        /// <summary>
+        /// 执行标量语句
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static object ExecuteScalar(string sql, SqlParameter[] parameters = null)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    if (parameters != null && parameters.Count() > 0)
+                    {
+                        cmd.Parameters.AddRange(parameters);
+                    }
+                    return cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 获取DataSet,只用来执行查询结果比较小的sql
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static DataSet GetDataSet(string sql, SqlParameter[] parameters = null)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    if (parameters != null && parameters.Count() > 0)
+                    {
+                        cmd.Parameters.AddRange(parameters);
+                    }
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataSet dataset = new DataSet();
+                    adapter.Fill(dataset);
+                    return dataset;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 获取DataTable,防止sql注入漏洞攻击
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static DataTable GetDataTable(string sql, SqlParameter[] parameters = null)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+
+                    if (parameters != null && parameters.Count() > 0)
+                    {
+                        cmd.Parameters.AddRange(parameters);
+                    }
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataSet dataset = new DataSet();
+                    adapter.Fill(dataset);
+
+                    return dataset.Tables[0];
+                }
+            }
+        }
+
+    }
+}
